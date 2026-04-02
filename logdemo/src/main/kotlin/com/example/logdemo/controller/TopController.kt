47fcc6b3890095445abf21423dfcf797a34bdee9
kuboshu83 <dot.sohkubo@gmail.com>
2026-04-02 23:01:45 +0900

    アプリ名を環境変数で設定できるようにした

--- logdemo/src/main/kotlin/com/example/logdemo/controller/TopController.kt
+++ logdemo/src/main/kotlin/com/example/logdemo/controller/TopController.kt
@@ -1,14 +1,20 @@
 package com.example.logdemo.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/")
-class TopController {
+class TopController(
+    @Value("app.name") private val appName: String
+) {
+
     @GetMapping
-    fun topPage(): String {
+    fun topPage(model: Model): String {
+        model.addAttribute("appName", appName)
         return "index"
     }
 
