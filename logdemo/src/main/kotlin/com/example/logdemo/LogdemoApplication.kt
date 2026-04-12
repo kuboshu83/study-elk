6d1401713b66e889b03541476b0c331fb66c1b09
kuboshu83 <dot.sohkubo@gmail.com>
2026-04-13 07:46:47 +0900

    ログにjob_idを追加

--- logdemo/src/main/kotlin/com/example/logdemo/LogdemoApplication.kt
+++ logdemo/src/main/kotlin/com/example/logdemo/LogdemoApplication.kt
@@ -1,11 +1,15 @@
 package com.example.logdemo
 
+import org.slf4j.MDC
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class LogdemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<LogdemoApplication>(*args)
+    val uuid = UUID.randomUUID().toString()
+    MDC.put("labels.job_id", uuid)
+    runApplication<LogdemoApplication>(*args)
 }
